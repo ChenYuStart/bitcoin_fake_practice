c311b8d62b93eba73c2e1b8742843f12cb70fa21
chany <chenyustart@gmail.com>
2023-12-08 13:54:43 +0800

    v2.0

--- target/debug/deps/main-7e12fa4c4d8adae3.d
+++
@@ -1,5 +0,0 @@
-F:\RustExerciseProject\bitcoin_practice\target\debug\deps\libmain-7e12fa4c4d8adae3.rmeta: main.rs
-
-F:\RustExerciseProject\bitcoin_practice\target\debug\deps\main-7e12fa4c4d8adae3.d: main.rs
-
-main.rs:
